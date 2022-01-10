80528c3d99eb0350792bf79bc7372aacea2ac62f
ismoil1428@gmail.com <ismoil1428@gmail.com>
2022-01-10 16:49:36 +0500

    toke  EXPIRATION_TIME  reduced to an hour

--- src/main/java/uz/gigalab/utilities/security/SecurityConstants.java
+++ src/main/java/uz/gigalab/utilities/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package uz.gigalab.utilities.security;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 864_000_00; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String SIGN_UP_URL = "/api/authenticate";
